18dafd672935c28b6a8711d7ce3f3889aa6f6106
Carter Johnson <carter.bland2000@gmail.com>
2026-03-30 10:18:04 -0500

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let leftProducts = [];
+        let rightProducts = [];
+        let result = [];
+
+        for (let i = 0; i < nums.length; i++) {
+            let leftProduct = i === 0 ? 1 : leftProducts[i-1] * nums[i - 1];
+            let rightProduct = i === 0 ? 1 : rightProducts[i-1] * nums[nums.length - i];
+            
+            leftProducts.push(leftProduct);
+            rightProducts.push(rightProduct);
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            result.push(leftProducts[i] * rightProducts[nums.length - 1 - i]);
+        }
+
+        return result;
+    }
+}
